fa085f9ad17a63954c84da1ec5afee36ef82fab3
fdodino <fernando.dodino@gmail.com>
2025-04-16 20:55:02 -0300

    Agrego precioTotalFactura para contemplar recargos

--- src/Library.hs
+++ src/Library.hs
@@ -6,7 +6,10 @@ doble numero = numero + numero
 
 -- Todas las facturas vencen el dia 15. Hacer una funcion que me indique si la factura se encuentra vencida al momento de pagarla
 facturaVencida :: Number -> Bool
-facturaVencida fechaPago 
+facturaVencida fechaPago
         | fechaPago >= 15 = True
         | otherwise  = False
 
+precioTotalFactura :: Number -> Number -> Number
+precioTotalFactura fechaPago totalOriginal =
+        totalOriginal * if facturaVencida fechaPago then 1.1 else 1
