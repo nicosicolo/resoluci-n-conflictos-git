e558dd1320b60cd13c1ceceffaccd3e41da2e8cf
Nicolas Sicolo <sicolo2208@gmail.com>
2025-04-16 20:49:47 -0300

    Cambio funcion facturaVencida

--- src/Library.hs
+++ src/Library.hs
@@ -6,7 +6,7 @@ doble numero = numero + numero
 
 -- Todas las facturas vencen el dia 15. Hacer una funcion que me indique si la factura se encuentra vencida al momento de pagarla
 facturaVencida :: Number -> Bool
-facturaVencida fechaPago 
-        | fechaPago >= 15 = True
-        | otherwise  = False
+facturaVencida fechaPago = fechaPago >= 15
 
+
+-- TO DO: precioTotalFactura - si la factura está vencida se agrega un 10% al total del importe
--- src/Spec.hs
+++ src/Spec.hs
@@ -8,4 +8,33 @@ correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
+  describe "Tests pago factura" $ do
+    it "Quiero pagar la factura antes de su vencimiento" $ do
+      facturaVencida 11 `shouldBe` False
+    it "Quiero pagar la factura después de su vencimiento" $ do
+      facturaVencida 17 `shouldBe` True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    {- it "Pago una factura antes de la fecha de vencimiento" $ do
+      precioTotalFactura 11 1000 `shouldBe` 1000
+    it "Pago una factura después de la fecha de vencimiento" $ do
+      precioTotalFactura 17 1000 `shouldBe` 1100 -}
 
